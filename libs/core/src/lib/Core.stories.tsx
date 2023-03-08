6d486a8eade8556bb6496622962eb8f47394db6d
kaustubh <kaustubh.zoratrox@gmail.com>
2023-03-08 23:00:56 +0530

    Adding Storybook

---
+++ libs/core/src/lib/Core.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { Core } from './Core';
+
+const Story: Meta<typeof Core> = {
+  component: Core,
+  title: 'Core',
+};
+export default Story;
+
+export const NamedStory = {
+  args: {},
+};
